c9266dd83bed5e528b5a8d7724d97359612b556c
Yarias2106 <Zekrom2110@gmail.com>
2021-10-12 15:45:13 -0400

    Agregando proyecto django

---
+++ Acoustic_Live/Acoustic_Live/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Acoustic_Live project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Acoustic_Live.settings')
+
+application = get_asgi_application()
