bfa192d35c71793d7da05747a009bf11ede4dcc6
Dachv1999 <Dachv1999@gmail.com>
2021-10-13 15:38:54 -0400

    Converion de Vista Niveles a Django

--- Acoustic_Live/Acoustic_Live/views.py
+++ Acoustic_Live/Acoustic_Live/views.py
@@ -3,7 +3,7 @@ from django.template import Template, Context
 
 
 def niveles(request): #Vista niveles
-    doc_externo = open("Acoustic_Live/Templates/Vista_Niveles.html")
+    doc_externo = open("Acoustic_Live/Templates/Division_Niveles.html")
     plt = Template(doc_externo.read())
     doc_externo.close()
 
