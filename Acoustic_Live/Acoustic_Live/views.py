a0e409524bb5aaaa224cc326fab67c59fff7872e
Dachv1999 <Dachv1999@gmail.com>
2021-10-12 20:33:57 -0400

    unir HTML con django

--- Acoustic_Live/Acoustic_Live/views.py
+++ Acoustic_Live/Acoustic_Live/views.py
@@ -11,3 +11,13 @@ def niveles(request): #Vista niveles
     documento = plt.render(ctx)
     
     return HttpResponse(documento)
+
+def nivel_principiante(request): #Vista nivel principiante
+    doc_externo = open("Acoustic_Live/Templates/Vista_Nivel_Principiante.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+
+    ctx = Context()
+    documento = plt.render(ctx)
+    
+    return HttpResponse(documento)
