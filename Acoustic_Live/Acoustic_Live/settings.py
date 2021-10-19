3d561c9a4db5a49af1920bee05be46c8abe6f775
luispanizado <201702077@est.umss.edu>
2021-10-19 19:08:51 -0400

    Cambiando la base a mysql y conexion con django

--- Acoustic_Live/Acoustic_Live/settings.py
+++ Acoustic_Live/Acoustic_Live/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'Acoustic_Live.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'acousticlive',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
+
 
 
 # Password validation
