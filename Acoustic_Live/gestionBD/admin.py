c0156bce5d522c364d4d3f6155233c34026c40b1
cris1198 <chiri.cristian6@gmail.com>
2021-10-21 12:05:28 -0400

    llenar datos de las tablas base de datos

--- Acoustic_Live/gestionBD/admin.py
+++ Acoustic_Live/gestionBD/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Leccion, Profesor
 # Register your models here.
+admin.site.register(Leccion)
+admin.site.register(Profesor)
--- Acoustic_Live/gestionBD/models.py
+++ Acoustic_Live/gestionBD/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.db.models.fields import TextField
+from django.db.models.deletion import CASCADE
+
 # Create your models here.
 class Profesor(models.Model):
     nombre_profesor = models.CharField(max_length=50)
-    email = models.EmailField()
-    user_name = models.CharField(max_length=20)
-    contrasenia = models.CharField(max_length=20)
+    apellido_profesor = models.CharField(max_length=50)
+    email = models.CharField(max_length=80)
+    user_name = models.CharField(max_length=50)
+    contrasenia = models.CharField(max_length=50)
 
 class Leccion(models.Model):
-    nombre_leccion = models.CharField(max_length=50)
+    nombre_leccion = models.CharField(max_length=100)
     nivel =models.IntegerField()
     link = models.TextField()
     descripcion = models.TextField()
-    numero_clase = models.IntegerField()
+    idprofesor = models.ForeignKey(Profesor,on_delete=CASCADE)
