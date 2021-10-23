1193739da65d73d671790c7c8e7be80a7668ea9d
paolamfz <201803646@est.umss.edu>
2021-10-22 22:46:21 -0500

    Validacion datos del formulario

--- Acoustic_Live/Acoustic_Live/views.py
+++ Acoustic_Live/Acoustic_Live/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.template import Template, Context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from gestionBD.models import Leccion, Profesor
+
 
 
 def niveles(request): #Vista niveles
@@ -36,16 +37,6 @@ def nivel_avanzado(request): #Vista nivel avanzado
     
     return HttpResponse(documento)
 
-def formulario_nuevoVideo(request):
-    doc_externo = open("Acoustic_Live/Templates/formulario.html")
-    plt = Template(doc_externo.read())
-    doc_externo.close()
-
-    ctx = Context()
-    documento = plt.render(ctx)
-
-    return HttpResponse(documento)
-
 def profesoresNP(request): #Vista profesoresNP
     doc_externo = open("Acoustic_Live/Templates/Vista_Profesores_NP.html")
     plt = Template(doc_externo.read())
@@ -76,20 +67,38 @@ def profesoresNA(request): #Vista profesoresNA
 
     return HttpResponse(documento)
 
-def envio_formulario(request):
+def formulario_nuevoVideo(request):
 
-    nombre=request.POST.get('nombre','')
-    descripcion= request.POST.get('descripcion','')
-    link= request.POST.get('link','')
-    nivel= request.POST.get('nivel','')
+    if request.method=="POST":
+        nombre=request.POST.get('nombre','')
+        descripcion= request.POST.get('descripcion','')
+        link= request.POST.get('link','')
+        niveles= request.POST.get('nivel','')
 
-    ventana_a_mostrar=" "
-    
-    if (len(nombre) == 0 or len(descripcion) == 0 or len(link) == 0 or len(nivel) == 0 ):
-         ventana_a_mostrar="ventana_error.html"   #
-    else:
-        ventana_a_mostrar="ventana_correcta.html"
-        lecc=Leccion(nombre_leccion = nombre, nivel=nivel,link=link, descripcion = descripcion, idprofesor_id =1 )
-        lecc.save()
- 
-    return render (request, ventana_a_mostrar)
+        lecciones = Leccion.objects.filter(nivel=niveles, idprofesor_id=1)
+
+        if(len(nombre) > 50):
+            return redirect("/formulario/?error_nombre_muy_grande")
+
+        if(len(descripcion)>500):
+            return redirect("/formulario/?error_descripcion_muy_grande")
+
+        if(len(descripcion)<20 and len(descripcion)>0):
+            return redirect("/formulario/?error_descripcion_muy_pequeña")
+
+        if ((len(nombre)) != 0 and (len(descripcion)) != 0 and (len(link)) != 0 and (len(niveles)) != 0 ):
+            hayVideo=False
+            for leccion in lecciones:
+                if hayVideo==False:
+                    if leccion.link==link:
+                        hayVideo=True
+            if hayVideo==True:  
+                return redirect("/formulario/?videoExiste")
+            else:
+                # lecc=Leccion(nombre_leccion = nombre, nivel=niveles,link=link, descripcion = descripcion, idprofesor_id =1 )
+                # lecc.save()
+                return redirect("/formulario/?VideoGuardado")
+        else:
+            return redirect("/formulario/?Alguno_Esta_Vacio")
+            
+    return render (request, "formulario.html")
