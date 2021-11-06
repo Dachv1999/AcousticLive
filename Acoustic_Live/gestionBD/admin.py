448447f3b5f01307fe5d5acd3acefdd43f068ede
cris1198 <chiri.cristian6@gmail.com>
2021-11-06 18:11:51 -0400

    correcion de la base de datos

--- Acoustic_Live/gestionBD/admin.py
+++ Acoustic_Live/gestionBD/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Leccion, Profesor
+from .models import Cursa, Estudiante, Leccion, Profesor
 # Register your models here.
 admin.site.register(Leccion)
 admin.site.register(Profesor)
+admin.site.register(Estudiante)
+admin.site.register(Cursa)
