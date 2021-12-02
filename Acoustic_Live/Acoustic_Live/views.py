64b29c9e074868c7ab71f6c64fefef50702d4bef
cris1198 <chiri.cristian6@gmail.com>
2021-12-02 12:44:50 -0400

    dinamico la parte de canciones

--- Acoustic_Live/Acoustic_Live/views.py
+++ Acoustic_Live/Acoustic_Live/views.py
@@ -1028,25 +1028,26 @@ def Vista_Universal_Para_Profesor(request, id_profesor, nivel):
 #@user_passes_test(profesor1,login_url='/Inicio_Profesores/')
 def genero(request, num_genero):
     
-    texto = " "
+    texto =""
     if num_genero == 1:
         texto = 'Todas las canciones'
         canciones = Cancion.objects.all().order_by('nombre_cancion')
     elif num_genero == 2:
         texto = 'Género "Rock"'
-        canciones = Cancion.objects.filter(genero_musica='rock')
+        canciones = Cancion.objects.filter(genero_musica='Rock')
     elif num_genero == 3:
         texto = 'Género "Pop"'
-        canciones = Cancion.objects.filter(genero_musica='pop')
+        canciones = Cancion.objects.filter(genero_musica='Pop')
     elif num_genero == 4:
         texto = 'Género "Baladas"'
-        canciones = Cancion.objects.filter(genero_musica='balada')
+        canciones = Cancion.objects.filter(genero_musica='Balada')
     elif num_genero == 5:
         texto = 'Género "Folklóricas"'
-        canciones = Cancion.objects.filter(genero_musica='folklorica')
+        canciones = Cancion.objects.filter(genero_musica='Folklorica')
     elif num_genero == 6:
         texto = 'Género "Reggae"'
-        canciones = Cancion.objects.filter(genero_musica='reggae')
+        canciones = Cancion.objects.filter(genero_musica='Reggae')
+
     contexto = {
         'texto' : texto,
         'canciones':canciones,
@@ -1070,12 +1071,37 @@ def seccion_canciones(request): #Vista de seccion canciones
     
     return HttpResponse(documento)
 
-def cancion_base(request): #Base de las canciones
+def cancion_base(request,id_cancion): #Base de las canciones
     doc_externo = open("Acoustic_Live/Templates/Cancion_base.html")
     plt = Template(doc_externo.read())
     doc_externo.close()
-
-    ctx = Context()
-    documento = plt.render(ctx)
     
+    if id_cancion>6:
+        return render(request,'p.html')
+    else:
+        cancion= Cancion.objects.get(id=id_cancion)
+        i=cancion.acordes_imagenes
+        imagenes =i.split() 
+        print(imagenes)
+        n=cancion.acordes_nombres
+        nombre =n.split()     
+        
+        acordes_verdaderas=[] 
+        i=0
+        while i< len(imagenes):
+            acordes_verdaderas.append(Fotos(nombre[i],imagenes[i]))
+            i +=1
+        contexto = {
+            'cancion':cancion,
+            'acorde_normal':acordes_verdaderas,
+        }
+        
+        ctx = Context(contexto)
+    
+    documento = plt.render(ctx)
     return HttpResponse(documento)
+
+class Fotos(object):
+    def __init__(self, nn,ii):
+        self.i ='static/acordes_guitarra/'+ii
+        self.n =nn
